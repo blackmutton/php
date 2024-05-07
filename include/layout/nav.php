69e357437e5a24fa52efeff116d696b84b8779c3
yt Chen <yutongc30@gmail.com>
2024-05-07 14:09:59 +0800

    include的功能測試

---
+++ include/layout/nav.php
@@ -0,0 +1,8 @@
+<nav>
+        <a href="index.php">首頁</a>
+        <a href="news.php">最新消息</a>
+        <a href="products.php">產品介紹</a>
+        <a href="contact.php">聯絡我們</a>
+        <a href="guestbook.php">留言互動</a>
+        <a href="email.php">電子郵件</a>
+</nav>
